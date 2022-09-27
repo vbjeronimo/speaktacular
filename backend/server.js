2558202dd02bb1506e0ed211b519e16e3c89f81e
Vitor Jeronimo <vitor.bently@hotmail.com>
2022-09-26 21:33:05 -0300

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Connected...')
+})
+
+app.listen(PORT, () => {
+  console.log('Listening on port 5000...');
+})
+

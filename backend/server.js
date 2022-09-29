336793764d2e17d8ce171297ee371f9a94605eb8
Vitor Jeronimo <vitor.bently@hotmail.com>
2022-09-28 21:28:47 -0300

    Add CORS to the server file

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 
 const connectDB = require('./config/dbConnection');
+const corsOptions = require('./config/corsOptions');
 const { requestLogger } = require('./middleware/logger');
 
 const app = express();
@@ -11,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(requestLogger);

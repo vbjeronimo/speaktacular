79cb877220c01b837d9f9a298db02198e58816e0
Vitor Jeronimo <vitor.bently@hotmail.com>
2022-09-28 21:15:08 -0300

    Add database connection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 require('dotenv').config()
 const express = require('express');
+const mongoose = require('mongoose');
+
+const connectDB = require('./config/dbConnection');
+const { requestLogger } = require('./middleware/logger');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const { requestLogger } = require('./middleware/logger');
+connectDB();
 
 // Middleware
 app.use(express.json());
@@ -13,4 +18,6 @@ app.use(requestLogger);
 // Routes
 app.use('/api/practice-text', require('./routes/api/practiceText'));
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+mongoose.connection.once('open', () => {
+  app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+});

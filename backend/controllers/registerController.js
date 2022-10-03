ec158b3062abdf4f69b299301245384143765860
Vitor Jeronimo <vitor.bently@hotmail.com>
2022-10-03 19:26:13 -0300

    Fix issue with existing accounts

--- backend/controllers/registerController.js
+++ backend/controllers/registerController.js
@@ -4,27 +4,29 @@ const express = require('express');
 const User = require('../models/User');
 
 const handleNewUser = async (req, res) => {
-  const { emailAddress, username, password } = req.body;
+  try {
+    const { emailAddress, username, password } = req.body;
 
-  User.findOne({ emailAddress: emailAddress }, function (err, user) {
-    if (err) return res.status(500).json({ "message": err.message })
-    if (user) return res.status(409).json({ "message": `An account already exists with the email address "${emailAddress}"` })
-  });
-  User.findOne({ username: username }, function (err, user) {
-    if (err) return res.status(500).json({ "message": err.message })
-    if (user) return res.status(409).json({ "message": `An account already exists with username "${username}"` })
-  });
+    let user = await User.findOne({ emailAddress: emailAddress });
+    if (user) {
+      return res.status(409).json({ "message": `An account already exists with the email address "${emailAddress}"` })
+    }
+    user = await User.findOne({ username: username });
+    if (user) {
+      return res.status(409).json({ "message": `An account already exists with username "${username}"` })
+    }
 
-  const hashedPassword = await bcrypt.hash(password, 10);
-  User.create({
-    "emailAddress": emailAddress,
-    "username": username,
-    "password": hashedPassword
-  },
-  function (err, newUser) {
-    if (err) return res.status(500).json({ "message": err.message })
+    const hashedPassword = await bcrypt.hash(password, 10);
+    User.create({
+      "emailAddress": emailAddress,
+      "username": username,
+      "password": hashedPassword
+    });
+
     return res.status(201).json({ "success": `Account ${username} created` })
-  });
+  } catch (err) {
+    return res.status(500).json({ "message": err.message })
+  }
 }
 
 module.exports = { handleNewUser }
